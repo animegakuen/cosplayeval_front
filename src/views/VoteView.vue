d28edf63b9d84de39b4e271bc42a56efb719b2dd
thiagooo14 <thiagooo15@gmail.com>
2023-06-17 01:55:40 -0300

    style: css impruvement

--- src/views/VoteView.vue
+++ src/views/VoteView.vue
@@ -25,28 +25,35 @@ const onSubmit = () => {
   router.push({ path: `/vote/${number + 1}` })
 }
 
+const onSkip = () => {
+  const number = Number.parseInt(id)
+  router.push({ path: `/vote/${number + 1}` })
+}
+
 </script>
 
 <template>
   <main class="login">
-    <div v-if="data" class="form">
+    <div class="info">
       <h1>
-        cosplayer:
+        Cosplayer:
       </h1>
-      <p id="cosplayerNumber">{{ data?.id }}</p>
-      <p>{{ data?.name }}</p>
+      <p id="cosplayerNumber">{{ data?.id }} - {{ data?.name }}</p>
+      <!-- <p>{{ data?.name }}</p> -->
       <h1>Cosplay:</h1>
       <p>{{ data?.characterName }}</p>
-      <h4>
+    </div>
+    <div v-if="data" class="form">
+      <h2>
         Nota cosplayer:
-      </h4>
+      </h2>
       <input id="cosplayerVote" type="number" min="1" max="10" class="cosplayerVote" />
 
       <button type="submit" @click="onSubmit" class="submit">
         Enviar
       </button>
 
-      <button type="submit" class="submit">
+      <button type="submit" @click="onSkip" class="submit">
         Pular
       </button>
     </div>
@@ -56,21 +63,32 @@ const onSubmit = () => {
 
 <style scoped>
 .login {
+  /* background-color: red; */
   display: flex;
   height: 100vh;
   width: 100vw;
-
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
+.info{
+  text-align: center;
+}
+
+
 .form {
   display: flex;
+  margin: 3px;
   height: 20%;
-  width: 60%;
+  width: 60vw;
 
   flex-direction: column;
   align-items: center;
+}
+
+.form h2{
+  margin: 5px;
 }
 
 .form input {
@@ -81,6 +99,11 @@ const onSubmit = () => {
 .submit {
   border: none;
   height: 45px;
+  width: 55vw;
+  border-bottom: #cacaca 1px solid;
+  border-left: #cacaca 1px solid;
+  margin-bottom: 3px;
+  border-radius: 3%;
 }
 
 .end {
